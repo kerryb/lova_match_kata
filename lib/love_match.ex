fbbfa35d67932153164c50651ba1429658326767
Kerry Buckley <kerryjbuckley@gmail.com>
2021-02-11 09:43:35 +0000

    New project

---
+++ lib/love_match.ex
@@ -0,0 +1,18 @@
+defmodule LoveMatch do
+  @moduledoc """
+  Documentation for `LoveMatch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoveMatch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/love_match_test.exs
@@ -0,0 +1,8 @@
+defmodule LoveMatchTest do
+  use ExUnit.Case
+  doctest LoveMatch
+
+  test "greets the world" do
+    assert LoveMatch.hello() == :world
+  end
+end
