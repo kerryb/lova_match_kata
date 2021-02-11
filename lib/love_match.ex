f39b83bea49bd0af668468fb712366551d1b4a07
Kerry Buckley <kerryjbuckley@gmail.com>
2021-02-11 10:56:50 +0000

    Remove generated example code

--- lib/love_match.ex
+++ lib/love_match.ex
@@ -1,18 +1,2 @@
 defmodule LoveMatch do
-  @moduledoc """
-  Documentation for `LoveMatch`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LoveMatch.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/love_match_test.exs
+++ test/love_match_test.exs
@@ -1,8 +1,4 @@
 defmodule LoveMatchTest do
   use ExUnit.Case
   doctest LoveMatch
-
-  test "greets the world" do
-    assert LoveMatch.hello() == :world
-  end
 end
